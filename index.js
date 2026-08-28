d95dc55700d35ec8f749144a148ef8ef2ebd070e
mpaulo4533-svg <mpaulo4533@gmail.com>
2026-08-28 20:50:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // ============================================================
-// BOT STEAM FAMÍLIA - VERSÃO COMPLETA COM OPENAI + DM
+// BOT STEAM FAMÍLIA - VERSÃO COMPLETAa COM OPENAI + DM
 // ============================================================
 
 console.log('========================================');
